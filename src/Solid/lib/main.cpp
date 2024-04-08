491a88565970e09ff131672c640b9352904178e0
desktopgame <gnndr36@gmail.com>
2024-04-08 22:51:35 +0900

    Update: ウィンドウの表示を確認

--- src/Solid/lib/main.cpp
+++ src/Solid/lib/main.cpp
@@ -1,8 +1,53 @@
+#include <Windows.h>
 #include <iostream>
 #include <main.hpp>
+#include <tchar.h>
+
+LRESULT WindowProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+    if (msg == WM_DESTROY) {
+        PostQuitMessage(0);
+        return 0;
+    }
+    return DefWindowProc(hwnd, msg, wparam, lparam);
+}
 
 int main(int argc, char* argv[])
 {
-    std::cout << "Hello,world!" << std::endl;
+    WNDCLASSEX w = {};
+    w.cbSize = sizeof(WNDCLASSEX);
+    w.lpfnWndProc = (WNDPROC)WindowProcedure;
+    w.lpszClassName = _T("DX12");
+    w.hInstance = GetModuleHandle(nullptr);
+    RegisterClassEx(&w);
+
+    RECT wrc = { 0, 0, 800, 600 };
+    AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+
+    HWND hwnd = CreateWindow(
+        w.lpszClassName,
+        _T("DX12"),
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        wrc.right - wrc.left,
+        wrc.bottom - wrc.top,
+        nullptr,
+        nullptr,
+        w.hInstance,
+        nullptr);
+    ShowWindow(hwnd, SW_SHOW);
+
+    MSG msg = {};
+    while (true) {
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+        if (msg.message == WM_QUIT) {
+            break;
+        }
+    }
+    UnregisterClass(w.lpszClassName, w.hInstance);
     return 0;
 }
