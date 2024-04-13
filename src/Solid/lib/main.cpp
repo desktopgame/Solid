dcca3c6e502eed7690d0f750d9c87bf227be13a0
desktopgame <gnndr36@gmail.com>
2024-04-13 23:10:02 +0900

    Update: ライブラリの機能を追加するように

--- src/Solid/lib/main.cpp
+++ src/Solid/lib/main.cpp
@@ -1,235 +1,24 @@
 #include <Graphics/Device.hpp>
-#include <Windows.h>
-#include <d3d12.h>
-#include <dxgi1_6.h>
-#include <iostream>
-#include <main.hpp>
-#include <optional>
-#include <stdexcept>
-#include <string>
-#include <tchar.h>
-#include <vector>
-#include <wrl/client.h>
-
-LRESULT WindowProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
-{
-    if (msg == WM_DESTROY) {
-        PostQuitMessage(0);
-        return 0;
-    }
-    return DefWindowProc(hwnd, msg, wparam, lparam);
-}
+#include <Graphics/Engine.hpp>
+#include <Graphics/Window.hpp>
 
 int main(int argc, char* argv[])
 {
-    WNDCLASSEX w = {};
-    w.cbSize = sizeof(WNDCLASSEX);
-    w.lpfnWndProc = (WNDPROC)WindowProcedure;
-    w.lpszClassName = _T("DX12");
-    w.hInstance = GetModuleHandle(nullptr);
-    RegisterClassEx(&w);
-
-    RECT wrc = { 0, 0, 800, 600 };
-    AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
-
-    HWND hwnd = CreateWindow(
-        w.lpszClassName,
-        _T("DX12"),
-        WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT,
-        CW_USEDEFAULT,
-        wrc.right - wrc.left,
-        wrc.bottom - wrc.top,
-        nullptr,
-        nullptr,
-        w.hInstance,
-        nullptr);
-    ShowWindow(hwnd, SW_SHOW);
-
-    using Microsoft::WRL::ComPtr;
-    // Debug Layer
-    ComPtr<ID3D12Debug> debugController = nullptr;
-    if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController)))) {
-        debugController->EnableDebugLayer();
-    }
-    // Factory
-    ComPtr<IDXGIFactory6> dxgiFactory = nullptr;
-    UINT flags = DXGI_CREATE_FACTORY_DEBUG;
-    if (FAILED(CreateDXGIFactory2(flags, IID_PPV_ARGS(&dxgiFactory)))) {
-        throw std::runtime_error("failed CreateDXGIFactory2()");
-    }
-    // Adapter
-    std::vector<ComPtr<IDXGIAdapter>> adapters;
-    ComPtr<IDXGIAdapter> mainAdapter = nullptr;
-    for (uint32_t i = 0; dxgiFactory->EnumAdapters(i, &mainAdapter) != DXGI_ERROR_NOT_FOUND; i++) {
-        adapters.emplace_back(mainAdapter);
-    }
-    if (adapters.empty()) {
-        throw std::runtime_error("failed EnumAdapters()");
-    }
-    // first adapter is primary adapter.
-    // see: https://learn.microsoft.com/ja-jp/windows/win32/api/dxgi/nf-dxgi-idxgifactory-enumadapters
-    mainAdapter = adapters.at(0);
-    adapters.clear();
-    // Feature level
-    std::optional<D3D_FEATURE_LEVEL> useLevel;
-    D3D_FEATURE_LEVEL levels[] = {
-        D3D_FEATURE_LEVEL_12_1,
-        D3D_FEATURE_LEVEL_12_0,
-    };
-    ComPtr<ID3D12Device> device = nullptr;
-    for (D3D_FEATURE_LEVEL level : levels) {
-        HRESULT deviceStatus = D3D12CreateDevice(mainAdapter.Get(), level, IID_PPV_ARGS(&device));
-        if (SUCCEEDED(deviceStatus)) {
-            useLevel = level;
-            break;
-        }
-    }
-    if (!useLevel) {
-        throw std::runtime_error("failed D3D12CreateDevice()");
-    }
-    // InfoQueue
-    ComPtr<ID3D12InfoQueue> infoQueue = nullptr;
-    if (FAILED(device->QueryInterface(__uuidof(ID3D12InfoQueue), &infoQueue))) {
-        throw std::runtime_error("failed ID3D12InfoQueue()");
-    }
-    // CommandAllocator
-    ComPtr<ID3D12CommandAllocator> commandAllocator = nullptr;
-    if (FAILED(device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&commandAllocator)))) {
-        throw std::runtime_error("failed CreateCommandAllocator()");
-    }
-    // CommandList
-    ComPtr<ID3D12GraphicsCommandList> commandList = nullptr;
-    if (FAILED(device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, commandAllocator.Get(), nullptr, IID_PPV_ARGS(&commandList)))) {
-        throw std::runtime_error("failed CreateCommandList()");
-    }
-    // CommandQueue
-    ComPtr<ID3D12CommandQueue> commandQueue = nullptr;
-    D3D12_COMMAND_QUEUE_DESC commandQueueDesc = {};
-    commandQueueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
-    commandQueueDesc.NodeMask = 0;
-    commandQueueDesc.Priority = D3D12_COMMAND_QUEUE_PRIORITY_NORMAL;
-    commandQueueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
-    if (FAILED(device->CreateCommandQueue(&commandQueueDesc, IID_PPV_ARGS(&commandQueue)))) {
-        throw std::runtime_error("failed CreateCommandQueue()");
-    }
-    // Swapchain
-    ComPtr<IDXGISwapChain4> swapchain = nullptr;
-    DXGI_SWAP_CHAIN_DESC1 swapchainDesc = {};
-    swapchainDesc.Width = 800;
-    swapchainDesc.Height = 600;
-    swapchainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-    swapchainDesc.Stereo = false;
-    swapchainDesc.SampleDesc.Count = 1;
-    swapchainDesc.SampleDesc.Quality = 0;
-    swapchainDesc.BufferUsage = DXGI_USAGE_BACK_BUFFER;
-    swapchainDesc.BufferCount = 2;
-    swapchainDesc.Scaling = DXGI_SCALING_STRETCH;
-    swapchainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
-    swapchainDesc.AlphaMode = DXGI_ALPHA_MODE_UNSPECIFIED;
-    swapchainDesc.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
-    if (FAILED(dxgiFactory->CreateSwapChainForHwnd(commandQueue.Get(), hwnd, &swapchainDesc, nullptr, nullptr, (IDXGISwapChain1**)swapchain.ReleaseAndGetAddressOf()))) {
-        throw std::runtime_error("failed CreateSwapChainForHwnd()");
-    }
-    // DescriptorHeap
-    D3D12_DESCRIPTOR_HEAP_DESC heapDesc = {};
-    heapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
-    heapDesc.NodeMask = 0;
-    heapDesc.NumDescriptors = 2;
-    heapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
-    ComPtr<ID3D12DescriptorHeap> rtvHeaps = nullptr;
-    if (FAILED(device->CreateDescriptorHeap(&heapDesc, IID_PPV_ARGS(&rtvHeaps)))) {
-        throw std::runtime_error("failed CreateDescriptorHeap()");
-    }
-    // RenderTargetView
-    std::vector<ComPtr<ID3D12Resource>> renderTargetViews(2);
-    D3D12_CPU_DESCRIPTOR_HANDLE handle = rtvHeaps->GetCPUDescriptorHandleForHeapStart();
-    for (uint32_t i = 0; i < swapchainDesc.BufferCount; i++) {
-        if (FAILED(swapchain->GetBuffer(i, IID_PPV_ARGS(&renderTargetViews.at(i))))) {
-            throw std::runtime_error("failed GetBuffer()");
-        }
-        device->CreateRenderTargetView(renderTargetViews.at(i).Get(), nullptr, handle);
-        handle.ptr += device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
-    }
-    // Fence
-    ComPtr<ID3D12Fence> fence = nullptr;
-    uint64_t fenceVal = 0;
-    if (FAILED(device->CreateFence(fenceVal, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&fence)))) {
-        throw std::runtime_error("failed CreateFence()");
-    }
-
-    Lib::Graphics::Device dev;
-    dev.hello();
-
-    MSG msg = {};
-    commandQueue->Signal(fence.Get(), ++fenceVal);
+    auto engine = Lib::Graphics::Engine::getInstance()->startup(argc, argv);
+    auto device = engine->getDevice();
+    auto window = engine->getWindow();
+    window->show();
+    device->notify();
     while (true) {
-        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-        if (msg.message == WM_QUIT) {
+        if (window->translateMessage()) {
             break;
         }
-        // Main loop
-        commandAllocator->Reset();
-        uint32_t backBufferIndex
-            = swapchain->GetCurrentBackBufferIndex();
-        // Barrier
-        D3D12_RESOURCE_BARRIER barrier = {};
-        barrier.Flags = D3D12_RESOURCE_BARRIER_FLAG_NONE;
-        barrier.Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
-        barrier.Transition.pResource = renderTargetViews.at(backBufferIndex).Get();
-        barrier.Transition.Subresource = 0;
-        barrier.Transition.StateBefore = D3D12_RESOURCE_STATE_PRESENT;
-        barrier.Transition.StateAfter = D3D12_RESOURCE_STATE_RENDER_TARGET;
-        commandList->ResourceBarrier(1, &barrier);
-
-        D3D12_CPU_DESCRIPTOR_HANDLE rtvHandle = rtvHeaps->GetCPUDescriptorHandleForHeapStart();
-        rtvHandle.ptr += backBufferIndex * device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
-        commandList->OMSetRenderTargets(1, &rtvHandle, true, nullptr);
-
-        float clearColor[] = { 1.0f, 1.0f, 0.0f, 1.0f };
-        commandList->ClearRenderTargetView(rtvHandle, clearColor, 0, nullptr);
-
-        commandList->Close();
-
-        ID3D12CommandList* cmdLists[] = { commandList.Get() };
-        commandQueue->ExecuteCommandLists(1, cmdLists);
-
-        commandAllocator->Reset();
-        commandList->Reset(commandAllocator.Get(), nullptr);
-
-        barrier.Transition.StateBefore = D3D12_RESOURCE_STATE_RENDER_TARGET;
-        barrier.Transition.StateAfter = D3D12_RESOURCE_STATE_PRESENT;
-        commandList->ResourceBarrier(1, &barrier);
-        swapchain->Present(1, 0);
-
+        device->render();
         // Wait events
-        if (fence->GetCompletedValue() != fenceVal) {
-            HANDLE evt = CreateEvent(nullptr, false, false, nullptr);
-            fence->SetEventOnCompletion(fenceVal, evt);
-            WaitForSingleObject(evt, INFINITE);
-            CloseHandle(evt);
-        }
-
+        device->waitEvents();
         // Show messages
-        uint64_t messageCount = infoQueue->GetNumStoredMessages();
-        for (uint64_t i = 0; i < messageCount; i++) {
-            size_t messageSize = 0;
-            if (SUCCEEDED(infoQueue->GetMessage(i, nullptr, &messageSize))) {
-                D3D12_MESSAGE* message = (D3D12_MESSAGE*)::malloc(messageSize);
-                if (SUCCEEDED(infoQueue->GetMessage(i, message, &messageSize))) {
-                    bool isInfo = message->Severity != D3D12_MESSAGE_SEVERITY_INFO && message->Severity != D3D12_MESSAGE_SEVERITY_MESSAGE;
-                    if (!isInfo) {
-                        std::cout << message->pDescription << std::endl;
-                    }
-                }
-                ::free(message);
-            }
-        }
-        infoQueue->ClearStoredMessages();
+        device->flushLogEntries();
     }
-    UnregisterClass(w.lpszClassName, w.hInstance);
+    engine->shutdown();
     return 0;
 }
