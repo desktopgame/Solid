3b8cda54ed41edfb129764a22ddce4f272c42197
desktopgame <gnndr36@gmail.com>
2024-04-21 18:24:41 +0900

    Update: スプライト描画テスト

--- src/App/lib/main.cpp
+++ src/App/lib/main.cpp
@@ -13,26 +13,30 @@ int main(int argc, char* argv[])
         struct Output {
             float4 svpos : SV_POSITION;
             float2 uv : TEXCOORD;
+            float4 color : COLOR;
         };
         cbuffer cbuff0 : register(b0) { matrix mat; }
+        cbuffer cbuff1 : register(b1) { float4 color; }
 
-        Output vsMain(float3 pos : POSITION, float2 uv : TEXCOORD) {
+        Output vsMain(float2 pos : POSITION, float2 uv : TEXCOORD) {
             Output output;
-            output.svpos = mul(mat, float4(pos, 1));
+            output.svpos = mul(mat, float4(pos, 0, 1));
             output.uv = uv;
+            output.color = color;
             return output;
         })",
         "vsMain", R"(
         struct Output {
             float4 svpos : SV_POSITION;
             float2 uv : TEXCOORD;
+            float4 color : COLOR;
         };
 
         Texture2D<float4> tex : register(t0);
         SamplerState smp : register(s0);
 
         float4 psMain(Output input) : SV_TARGET {
-            float4 col = float4(tex.Sample(smp, input.uv));
+            float4 col = float4(tex.Sample(smp, input.uv)) * input.color;
             return col;
         })",
         "psMain");
@@ -41,12 +45,12 @@ int main(int argc, char* argv[])
     const float top = 0.5;
     const float bottom = -0.5;
     auto vb = Lib::Graphics::Buffer::create();
-    const std::vector<Lib::Graphics::VertexData3D> verts(
-        { Lib::Graphics::VertexData3D(Lib::Math::Vector3({ left, bottom, 0 }), Lib::Math::Vector2({ 0.0f, 1.0f })),
-            Lib::Graphics::VertexData3D(Lib::Math::Vector3({ left, top, 0 }), Lib::Math::Vector2({ 0.0f, 0.0f })),
-            Lib::Graphics::VertexData3D(Lib::Math::Vector3({ right, bottom, 0 }), Lib::Math::Vector2({ 1.0f, 1.0f })),
-            Lib::Graphics::VertexData3D(Lib::Math::Vector3({ right, top, 0 }), Lib::Math::Vector2({ 1.0f, 0.0f })) });
-    vb->allocate(sizeof(Lib::Graphics::VertexData3D) * verts.size());
+    const std::vector<Lib::Graphics::VertexData2D> verts(
+        { Lib::Graphics::VertexData2D(Lib::Math::Vector2({ left, bottom }), Lib::Math::Vector2({ 0.0f, 1.0f })),
+            Lib::Graphics::VertexData2D(Lib::Math::Vector2({ left, top }), Lib::Math::Vector2({ 0.0f, 0.0f })),
+            Lib::Graphics::VertexData2D(Lib::Math::Vector2({ right, bottom }), Lib::Math::Vector2({ 1.0f, 1.0f })),
+            Lib::Graphics::VertexData2D(Lib::Math::Vector2({ right, top }), Lib::Math::Vector2({ 1.0f, 0.0f })) });
+    vb->allocate(sizeof(Lib::Graphics::VertexData2D) * verts.size());
     vb->update(verts.data());
 
     auto ib = Lib::Graphics::Buffer::create();
@@ -55,37 +59,25 @@ int main(int argc, char* argv[])
     ib->update(index.data());
 
     auto param = std::make_shared<Lib::Graphics::RenderParameter>();
-    auto rotate = Lib::Math::Matrix::rotateY(Lib::Math::Mathf::Deg2Rad * 45.0f);
-    auto pers = Lib::Math::Matrix::perspective(90.0f, Lib::Graphics::Screen::getAspectRatio(), 1, 10);
+    auto t = Lib::Math::Matrix::translate(Lib::Math::Vector3({ 0, 2, 0 }));
+    auto r = Lib::Math::Matrix ::rotateZ(Lib::Math::Mathf::Deg2Rad * 45.0f);
+    auto ortho = Lib::Math::Matrix::ortho(800, 600, -1, 1);
     auto view = Lib::Math::Matrix::lookAt(
-        Lib::Math::Vector3({ 0, 0, -5 }),
+        Lib::Math::Vector3({ 0, 0, -1 }),
         Lib::Math::Vector3({ 0, 0, 1 }),
         Lib::Math::Vector3({ 0, 1, 0 }));
-    // auto translate = Lib::Math::Matrix::translate(Lib::Math::Vector3({ 0, 0, 5 }));
-    // auto scale = Lib::Math::Matrix::scale(Lib::Math::Vector3({ 10, 10, 1 }));
-    param->setTransform(rotate * view * pers);
-    // param->setColor(Lib::Math::Vector4({ 1, 0, 1, 1 }));
+    auto scale = Lib::Math::Matrix::scale(Lib::Math::Vector3({ 100, 100, 1 }));
+    param->setTransform((scale * r * t) * view * ortho);
+    param->setColor(Lib::Math::Vector4({ 1, 1, 1, 1 }));
     param->setTexture(texture);
 
-    Lib::Math::Vector3 pt = Lib::Math::makeVector3(0, 0, 0);
-    Lib::Math::Vector3 pt2 = Lib::Math::Matrix::multiply(view * pers, pt);
-    std::cout << pt << " " << pt2 << std::endl;
-
     window->show();
-    float deg = 45.0f;
     while (true) {
         if (window->translateMessage()) {
             break;
         }
         surface->begin();
-        deg += 1.0f;
-        if (deg >= 360.0f) {
-            deg -= 360.0f;
-        }
-        rotate = Lib::Math::Matrix::rotateY(Lib::Math::Mathf::Deg2Rad * deg);
-        param->setTransform(rotate * view * pers);
-
-        surface->draw(shader, param, Lib::Graphics::PrimitiveType::Triangles, 3, true, vb, ib, 6);
+        surface->draw(shader, param, Lib::Graphics::PrimitiveType::Triangles, 2, true, vb, ib, 6);
         surface->end();
         // Show messages
         device->flushLogEntries();
