136da832e1cd617e36d5d0f3e2dc61541befebd5
desktopgame <gnndr36@gmail.com>
2024-08-25 15:20:49 +0900

    Update: ID3D12Resource を internal化

--- src/App/mod/Graphics/include/Graphics/Buffer.hpp
+++ src/App/mod/Graphics/include/Graphics/Buffer.hpp
@@ -1,6 +1,11 @@
 #pragma once
 #include <memory>
+
+#if SOLID_ENABLE_INTERNAL
+#include <d3d12.h>
+#include <dxgi1_6.h>
 #include <wrl/client.h>
+#endif
 
 namespace Lib::Graphics {
 class Buffer {
@@ -15,12 +20,18 @@ public:
     uint64_t getVirtualAddress() const;
     size_t getSize() const;
 
+#if SOLID_ENABLE_INTERNAL
+    Microsoft::WRL::ComPtr<ID3D12Resource> getID3D12Resource() const;
+#endif
+
 private:
     Buffer();
 
     size_t m_size;
 
-    class Impl;
-    std::shared_ptr<Impl> m_impl;
+#if SOLID_ENABLE_INTERNAL
+    Microsoft::WRL::ComPtr<ID3D12Device> m_device;
+    Microsoft::WRL::ComPtr<ID3D12Resource> m_resource;
+#endif
 };
 }
--- src/App/mod/Graphics/lib/Graphics/Buffer.cpp
+++ src/App/mod/Graphics/lib/Graphics/Buffer.cpp
@@ -1,24 +1,15 @@
 #include <Graphics/Buffer.hpp>
 #include <Graphics/Device.hpp>
 #include <Graphics/Engine.hpp>
-#include <d3d12.h>
-#include <dxgi1_6.h>
 #include <stdexcept>
 
 namespace Lib::Graphics {
 using Microsoft::WRL::ComPtr;
-// Impl
-class Buffer::Impl {
-public:
-    explicit Impl() = default;
-    ComPtr<ID3D12Device> device;
-    ComPtr<ID3D12Resource> resource;
-};
 // public
 std::shared_ptr<Buffer> Buffer::create()
 {
     auto buffer = std::shared_ptr<Buffer>(new Buffer());
-    buffer->m_impl->device = Engine::getInstance()->getDevice()->getID3D12Device();
+    buffer->m_device = Engine::getInstance()->getDevice()->getID3D12Device();
     return buffer;
 }
 
@@ -42,13 +33,13 @@ void Buffer::allocate(size_t size)
     resDesc.SampleDesc.Count = 1;
     resDesc.Flags = D3D12_RESOURCE_FLAG_NONE;
     resDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
-    if (FAILED(m_impl->device->CreateCommittedResource(
+    if (FAILED(m_device->CreateCommittedResource(
             &heapProps,
             D3D12_HEAP_FLAG_NONE,
             &resDesc,
             D3D12_RESOURCE_STATE_GENERIC_READ,
             nullptr,
-            IID_PPV_ARGS(&m_impl->resource)))) {
+            IID_PPV_ARGS(&m_resource)))) {
         throw std::runtime_error("failed CreateCommittedResource()");
     }
     m_size = size;
@@ -57,25 +48,30 @@ void Buffer::allocate(size_t size)
 void Buffer::update(const void* data)
 {
     void* outData;
-    if (FAILED(m_impl->resource->Map(0, nullptr, (void**)&outData))) {
+    if (FAILED(m_resource->Map(0, nullptr, (void**)&outData))) {
         throw std::runtime_error("failed Map()");
     }
     ::memcpy(outData, data, m_size);
-    m_impl->resource->Unmap(0, nullptr);
+    m_resource->Unmap(0, nullptr);
 }
 
 uint64_t Buffer::getVirtualAddress() const
 {
-    return static_cast<uint64_t>(m_impl->resource->GetGPUVirtualAddress());
+    return static_cast<uint64_t>(m_resource->GetGPUVirtualAddress());
 }
 size_t Buffer::getSize() const
 {
     return m_size;
 }
+// internal
+Microsoft::WRL::ComPtr<ID3D12Resource> Buffer::getID3D12Resource() const
+{
+    return m_resource;
+}
 // private
 Buffer::Buffer()
     : m_size(0)
-    , m_impl(std::make_shared<Impl>())
+    , m_resource()
 {
 }
 }
