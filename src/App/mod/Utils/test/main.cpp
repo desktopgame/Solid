136ec3c7cc38a6d634dcfde26c7c48af508a3989
desktopgame <gnndr36@gmail.com>
2024-09-18 09:28:31 +0900

    Update: interpolate のテストを追加

--- src/App/mod/Utils/test/main.cpp
+++ src/App/mod/Utils/test/main.cpp
@@ -3,7 +3,14 @@
 
 int main(int argc, char* argv[])
 {
-    assert(std::string("Hello") == Lib::Utils::String::toMultiByteString(L"Hello"));
-    assert(std::string("こんにちわ") == Lib::Utils::String::toMultiByteString(L"こんにちわ"));
+    using namespace Lib::Utils;
+    assert(std::string("Hello") == String::toMultiByteString(L"Hello"));
+    assert(std::string("こんにちわ") == String::toMultiByteString(L"こんにちわ"));
+
+    std::string oldText = "こんにちわ、${Name}さん";
+    std::string newText = "こんにちわ、dansakaさん";
+    std::unordered_map<std::string, std::string> keywords;
+    keywords.insert_or_assign("Name", "dansaka");
+    assert(String::interpolate(oldText, keywords) == newText);
     return 0;
 }
