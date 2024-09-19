159a13d80ab3524f9f3ffae2bd191a1db0b2df9b
desktopgame <gnndr36@gmail.com>
2024-09-19 11:28:19 +0900

    Add: テストを追加

--- src/App/mod/Utils/test/main.cpp
+++ src/App/mod/Utils/test/main.cpp
@@ -12,5 +12,17 @@ int main(int argc, char* argv[])
     std::unordered_map<std::string, std::string> keywords;
     keywords.insert_or_assign("Name", "dansaka");
     assert(String::interpolate(oldText, keywords) == newText);
+
+    std::basic_stringbuf<char> ibuf("1,2,3,4");
+    std::vector<Lib::Math::Vector4> v;
+    std::istream is(&ibuf);
+    IO::deserializeTile(is, v);
+    assert(v.size() == 1);
+    assert(v.at(0) == Lib::Math::Vector4({ 1, 2, 3, 4 }));
+
+    std::basic_stringbuf<char> obuf;
+    std::ostream os(&obuf);
+    IO::serializeTile(os, v);
+    assert(obuf.str() == "1,2,3,4\n");
     return 0;
 }
