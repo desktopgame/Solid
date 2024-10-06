58353a42c99e3a45123a295c95d1f7e1d8cd74b1
desktopgame <gnndr36@gmail.com>
2024-10-06 23:14:05 +0900

    Update: 新しい拡張子を指定可能に

--- scripts/mylib.py
+++ scripts/mylib.py
@@ -2,8 +2,9 @@ import pathlib
 from typing import Tuple, List
 
 
-def resolve_import_files(ext: str) -> List[Tuple[pathlib.Path, pathlib.Path]]:
+def resolve_import_files(ext: str, new_ext: str) -> List[Tuple[pathlib.Path, pathlib.Path]]:
     assert ext is not None or ext[0] == '.'
+    assert new_ext is not None or new_ext[0] == '.'
     rawdata = pathlib.Path('rawdata')
     assets = pathlib.Path('assets')
     ret: List[Tuple[pathlib.Path, pathlib.Path]] = []
@@ -11,6 +12,7 @@ def resolve_import_files(ext: str) -> List[Tuple[pathlib.Path, pathlib.Path]]:
     for file in rawdata.glob(f'**/*{ext}'):
         rel = file.relative_to(rawdata)
         asset = assets.joinpath(rel)
+        asset = pathlib.Path(asset.parent).joinpath(f'{asset.stem}{new_ext}')
         if not asset.parent.exists():
             asset.parent.mkdir(parents=True)
         ret.append((file, asset))
