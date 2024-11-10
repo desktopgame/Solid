236c8b47cfcc8bd48b8884ce94ebb35c2a31ae2f
desktopgame <gnndr36@gmail.com>
2024-11-10 13:38:41 +0900

    Add: 静的リフレクションのためのヘッダーを追加

---
+++ src/App/mod/Graphics/include/Graphics/Reflect.hpp
@@ -0,0 +1,50 @@
+#pragma once
+#include <Math/Vector.hpp>
+#include <vector>
+
+namespace Lib::Graphics::Reflect {
+enum InputLayout {
+    Vertex2D,
+    VertexTexCoord2D,
+    Vertex3D,
+    VertexTexCoord3D,
+    VertexNormalTexCoord3D
+};
+
+class UCamera {
+public:
+    Math::Matrix modelMatrix;
+    Math::Matrix viewMatrix;
+    Math::Matrix projectionMatrix;
+    std::array<Math::Vector4, 4> padding;
+};
+static_assert(sizeof(UCamera) == 256);
+
+class UFloat {
+public:
+    float value;
+    std::array<float, 63> padding;
+};
+static_assert(sizeof(UFloat) == 256);
+
+class UVector2 {
+public:
+    Math::Vector2 value;
+    std::array<float, 62> padding;
+};
+static_assert(sizeof(UVector2) == 256);
+
+class UVector3 {
+public:
+    Math::Vector3 value;
+    std::array<float, 61> padding;
+};
+static_assert(sizeof(UVector3) == 256);
+
+class UVector4 {
+public:
+    Math::Vector4 value;
+    std::array<float, 60> padding;
+};
+static_assert(sizeof(UVector4) == 256);
+}
