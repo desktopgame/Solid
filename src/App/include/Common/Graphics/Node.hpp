77bcf12c92d3e9356d43ec29b0ebea78748ce82d
desktopgame <gnndr36@gmail.com>
2024-11-30 10:10:48 +0900

    Update: setter/getter 用意

--- src/App/include/Common/Graphics/Node.hpp
+++ src/App/include/Common/Graphics/Node.hpp
@@ -5,19 +5,35 @@ namespace App::Common::Graphics {
 class Node : public std::enable_shared_from_this<Node> {
 public:
     explicit Node() = default;
-    std::array<char, 16> name;
-    Vector3 position;
-    Vector3 size;
-    Vector3 color;
-    std::vector<std::shared_ptr<Node>> children;
-    bool removed;
 
     static std::shared_ptr<Node> s_selected;
     static std::shared_ptr<Node> s_target;
 
     void update();
+    void removeFromParent();
+
     void inspect();
     void gui(const std::shared_ptr<Node>& parent);
     void draw(const std::shared_ptr<Node>& parent, const std::shared_ptr<Renderer>& renderer);
+
+    void setName(const std::array<char, 16>& name);
+    std::array<char, 16> getName() const;
+    std::array<char, 16>& getName();
+
+    void setPosition(const Vector3& position);
+    Vector3 getPosition() const;
+    Vector3& getPosition();
+
+    void setSize(const Vector3& size);
+    Vector3 getSize() const;
+    Vector3& getSize();
+
+private:
+    std::array<char, 16> m_name;
+    Vector3 m_position;
+    Vector3 m_size;
+    Vector3 m_color;
+    std::vector<std::shared_ptr<Node>> m_children;
+    bool m_removed;
 };
 }
--- src/App/lib/Common/Graphics/Node.cpp
+++ src/App/lib/Common/Graphics/Node.cpp
@@ -2,66 +2,68 @@
 #include <imgui.h>
 
 namespace App::Common::Graphics {
-// Node
 std::shared_ptr<Node> Node::s_selected = nullptr;
 std::shared_ptr<Node> Node::s_target = nullptr;
+// public
 void Node::update()
 {
-    for (auto& c : children) {
+    for (auto& c : m_children) {
         c->update();
     }
-    auto iter = std::remove_if(children.begin(), children.end(), [](const auto& e) -> bool {
+    auto iter = std::remove_if(m_children.begin(), m_children.end(), [](const auto& e) -> bool {
         return e->removed;
     });
-    children.erase(iter, children.end());
+    m_children.erase(iter, m_children.end());
 }
+
+void Node::removeFromParent() { m_removed = true; }
 
 void Node::inspect()
 {
     ImGui::SeparatorText("Layout");
     if (ImGui::Button("-PosX")) {
         if (s_selected) {
-            Vector3 pos = s_selected->position - (s_selected->size * Vector3({ 0.5f, 0, 0 }));
-            pos.x() -= size.x() * 0.5f;
-            position = pos;
+            Vector3 pos = s_selected->m_position - (s_selected->m_size * Vector3({ 0.5f, 0, 0 }));
+            pos.x() -= m_size.x() * 0.5f;
+            m_position = pos;
         }
     }
     ImGui::SameLine();
     if (ImGui::Button("+PosX")) {
         if (s_selected) {
-            Vector3 pos = s_selected->position + (s_selected->size * Vector3({ 0.5f, 0, 0 }));
-            pos.x() += size.x() * 0.5f;
-            position = pos;
+            Vector3 pos = s_selected->m_position + (s_selected->m_size * Vector3({ 0.5f, 0, 0 }));
+            pos.x() += m_size.x() * 0.5f;
+            m_position = pos;
         }
     }
     if (ImGui::Button("-PosY")) {
         if (s_selected) {
-            Vector3 pos = s_selected->position - (s_selected->size * Vector3({ 0, 0.5f, 0 }));
-            pos.y() -= size.y() * 0.5f;
-            position = pos;
+            Vector3 pos = s_selected->m_position - (s_selected->m_size * Vector3({ 0, 0.5f, 0 }));
+            pos.y() -= m_size.y() * 0.5f;
+            m_position = pos;
         }
     }
     ImGui::SameLine();
     if (ImGui::Button("+PosY")) {
         if (s_selected) {
-            Vector3 pos = s_selected->position + (s_selected->size * Vector3({ 0, 0.5f, 0 }));
-            pos.y() += size.y() * 0.5f;
-            position = pos;
+            Vector3 pos = s_selected->m_position + (s_selected->m_size * Vector3({ 0, 0.5f, 0 }));
+            pos.y() += m_size.y() * 0.5f;
+            m_position = pos;
         }
     }
     if (ImGui::Button("-PosZ")) {
         if (s_selected) {
-            Vector3 pos = s_selected->position - (s_selected->size * Vector3({ 0, 0, 0.5f }));
-            pos.z() -= size.z() * 0.5f;
-            position = pos;
+            Vector3 pos = s_selected->m_position - (s_selected->m_size * Vector3({ 0, 0, 0.5f }));
+            pos.z() -= m_size.z() * 0.5f;
+            m_position = pos;
         }
     }
     ImGui::SameLine();
     if (ImGui::Button("+PosZ")) {
         if (s_selected) {
-            Vector3 pos = s_selected->position + (s_selected->size * Vector3({ 0, 0, 0.5f }));
-            pos.z() += size.z() * 0.5f;
-            position = pos;
+            Vector3 pos = s_selected->m_position + (s_selected->m_size * Vector3({ 0, 0, 0.5f }));
+            pos.z() += m_size.z() * 0.5f;
+            m_position = pos;
         }
     }
 }
@@ -72,29 +74,29 @@ void Node::gui(const std::shared_ptr<Node>& parent)
     if (shared_from_this() == s_selected) {
         flags = ImGuiTreeNodeFlags_Selected;
     }
-    if (ImGui::TreeNodeEx(name.data(), flags)) {
+    if (ImGui::TreeNodeEx(m_name.data(), flags)) {
         if (ImGui::IsItemClicked()) {
             s_selected = shared_from_this();
         }
-        ImGui::InputText("Name", name.data(), 16);
-        ImGui::DragFloat3("Pos", position.data(), 0.01f);
-        ImGui::DragFloat3("Size", size.data(), 0.01f);
-        ImGui::ColorEdit3("Color", color.data());
+        ImGui::InputText("Name", m_name.data(), 16);
+        ImGui::DragFloat3("Pos", m_position.data(), 0.01f);
+        ImGui::DragFloat3("Size", m_size.data(), 0.01f);
+        ImGui::ColorEdit3("Color", m_color.data());
         if (ImGui::Button("New Node")) {
             auto child = std::make_shared<Node>();
-            child->position = Vector3({ 0, 0, 0 });
-            child->size = Vector3({ 10, 10, 10 });
-            child->color = Vector3({ 1, 1, 1 });
+            child->m_position = Vector3({ 0, 0, 0 });
+            child->m_size = Vector3({ 10, 10, 10 });
+            child->m_color = Vector3({ 1, 1, 1 });
 
             char buf[16];
-            ::sprintf(buf, "Child[%d]", static_cast<int32_t>(children.size()));
+            ::sprintf(buf, "Child[%d]", static_cast<int32_t>(m_children.size()));
             std::string childName = buf;
-            std::copy(childName.begin(), childName.end(), child->name.begin());
-            children.push_back(child);
+            std::copy(childName.begin(), childName.end(), child->m_name.begin());
+            m_children.push_back(child);
         }
         ImGui::SameLine();
         if (ImGui::Button("Remove This")) {
-            removed = true;
+            m_removed = true;
         }
         ImGui::SameLine();
         if (ImGui::Button("Inspect This")) {
@@ -102,7 +104,7 @@ void Node::gui(const std::shared_ptr<Node>& parent)
             s_selected = parent;
         }
 
-        for (const auto& c : children) {
+        for (const auto& c : m_children) {
             c->gui(shared_from_this());
         }
         ImGui::TreePop();
@@ -114,67 +116,79 @@ void Node::draw(const std::shared_ptr<Node>& parent, const std::shared_ptr<Rende
     float thickness = 0.25f;
     {
         Vector3 offset = Vector3({ thickness / 2.0f, thickness / 2.0f, 0 });
-        Vector3 center = position + (size * Vector3({ 0.5f, 0.5f, 0 })) - offset;
-        renderer->drawBox(center, Vector3({ thickness, thickness, size.z() }), Quaternion(), Vector4(color, 1), false);
+        Vector3 center = m_position + (m_size * Vector3({ 0.5f, 0.5f, 0 })) - offset;
+        renderer->drawBox(center, Vector3({ thickness, thickness, m_size.z() }), Quaternion(), Vector4(m_color, 1), false);
     }
     {
         Vector3 offset = Vector3({ -thickness / 2.0f, thickness / 2.0f, 0 });
-        Vector3 center = position + (size * Vector3({ -0.5f, 0.5f, 0 })) - offset;
-        renderer->drawBox(center, Vector3({ thickness, thickness, size.z() }), Quaternion(), Vector4(color, 1), false);
+        Vector3 center = m_position + (m_size * Vector3({ -0.5f, 0.5f, 0 })) - offset;
+        renderer->drawBox(center, Vector3({ thickness, thickness, m_size.z() }), Quaternion(), Vector4(m_color, 1), false);
     }
     {
         Vector3 offset = Vector3({ thickness / 2.0f, -thickness / 2.0f, 0 });
-        Vector3 center = position + (size * Vector3({ 0.5f, -0.5f, 0 })) - offset;
-        renderer->drawBox(center, Vector3({ thickness, thickness, size.z() }), Quaternion(), Vector4(color, 1), false);
+        Vector3 center = m_position + (m_size * Vector3({ 0.5f, -0.5f, 0 })) - offset;
+        renderer->drawBox(center, Vector3({ thickness, thickness, m_size.z() }), Quaternion(), Vector4(m_color, 1), false);
     }
     {
         Vector3 offset = Vector3({ -thickness / 2.0f, -thickness / 2.0f, 0 });
-        Vector3 center = position + (size * Vector3({ -0.5f, -0.5f, 0 })) - offset;
-        renderer->drawBox(center, Vector3({ thickness, thickness, size.z() }), Quaternion(), Vector4(color, 1), false);
+        Vector3 center = m_position + (m_size * Vector3({ -0.5f, -0.5f, 0 })) - offset;
+        renderer->drawBox(center, Vector3({ thickness, thickness, m_size.z() }), Quaternion(), Vector4(m_color, 1), false);
     }
     {
         Vector3 offset = Vector3({ thickness / 2.0f, 0, thickness / 2.0f });
-        Vector3 center = position + (size * Vector3({ 0.5f, 0, 0.5f })) - offset;
-        renderer->drawBox(center, Vector3({ thickness, size.y(), thickness }), Quaternion(), Vector4(color, 1), false);
+        Vector3 center = m_position + (m_size * Vector3({ 0.5f, 0, 0.5f })) - offset;
+        renderer->drawBox(center, Vector3({ thickness, m_size.y(), thickness }), Quaternion(), Vector4(m_color, 1), false);
     }
     {
         Vector3 offset = Vector3({ thickness / 2.0f, 0, -thickness / 2.0f });
-        Vector3 center = position + (size * Vector3({ 0.5f, 0, -0.5f })) - offset;
-        renderer->drawBox(center, Vector3({ thickness, size.y(), thickness }), Quaternion(), Vector4(color, 1), false);
+        Vector3 center = m_position + (m_size * Vector3({ 0.5f, 0, -0.5f })) - offset;
+        renderer->drawBox(center, Vector3({ thickness, m_size.y(), thickness }), Quaternion(), Vector4(m_color, 1), false);
     }
     {
         Vector3 offset = Vector3({ -thickness / 2.0f, 0, thickness / 2.0f });
-        Vector3 center = position + (size * Vector3({ -0.5f, 0, 0.5f })) - offset;
-        renderer->drawBox(center, Vector3({ thickness, size.y(), thickness }), Quaternion(), Vector4(color, 1), false);
+        Vector3 center = m_position + (m_size * Vector3({ -0.5f, 0, 0.5f })) - offset;
+        renderer->drawBox(center, Vector3({ thickness, m_size.y(), thickness }), Quaternion(), Vector4(m_color, 1), false);
     }
     {
         Vector3 offset = Vector3({ -thickness / 2.0f, 0, -thickness / 2.0f });
-        Vector3 center = position + (size * Vector3({ -0.5f, 0, -0.5f })) - offset;
-        renderer->drawBox(center, Vector3({ thickness, size.y(), thickness }), Quaternion(), Vector4(color, 1), false);
+        Vector3 center = m_position + (m_size * Vector3({ -0.5f, 0, -0.5f })) - offset;
+        renderer->drawBox(center, Vector3({ thickness, m_size.y(), thickness }), Quaternion(), Vector4(m_color, 1), false);
     }
     {
         Vector3 offset = Vector3({ 0, thickness / 2.0f, thickness / 2.0f });
-        Vector3 center = position + (size * Vector3({ 0, 0.5f, 0.5f })) - offset;
-        renderer->drawBox(center, Vector3({ size.x(), thickness, thickness }), Quaternion(), Vector4(color, 1), false);
+        Vector3 center = m_position + (m_size * Vector3({ 0, 0.5f, 0.5f })) - offset;
+        renderer->drawBox(center, Vector3({ m_size.x(), thickness, thickness }), Quaternion(), Vector4(m_color, 1), false);
     }
     {
         Vector3 offset = Vector3({ 0, -thickness / 2.0f, thickness / 2.0f });
-        Vector3 center = position + (size * Vector3({ 0, -0.5f, 0.5f })) - offset;
-        renderer->drawBox(center, Vector3({ size.x(), thickness, thickness }), Quaternion(), Vector4(color, 1), false);
+        Vector3 center = m_position + (m_size * Vector3({ 0, -0.5f, 0.5f })) - offset;
+        renderer->drawBox(center, Vector3({ m_size.x(), thickness, thickness }), Quaternion(), Vector4(m_color, 1), false);
     }
     {
         Vector3 offset = Vector3({ 0, thickness / 2.0f, -thickness / 2.0f });
-        Vector3 center = position + (size * Vector3({ 0, 0.5f, -0.5f })) - offset;
-        renderer->drawBox(center, Vector3({ size.x(), thickness, thickness }), Quaternion(), Vector4(color, 1), false);
+        Vector3 center = m_position + (m_size * Vector3({ 0, 0.5f, -0.5f })) - offset;
+        renderer->drawBox(center, Vector3({ m_size.x(), thickness, thickness }), Quaternion(), Vector4(m_color, 1), false);
     }
     {
         Vector3 offset = Vector3({ 0, -thickness / 2.0f, -thickness / 2.0f });
-        Vector3 center = position + (size * Vector3({ 0, -0.5f, -0.5f })) - offset;
-        renderer->drawBox(center, Vector3({ size.x(), thickness, thickness }), Quaternion(), Vector4(color, 1), false);
+        Vector3 center = m_position + (m_size * Vector3({ 0, -0.5f, -0.5f })) - offset;
+        renderer->drawBox(center, Vector3({ m_size.x(), thickness, thickness }), Quaternion(), Vector4(m_color, 1), false);
     }
 
-    for (const auto& c : children) {
+    for (const auto& c : m_children) {
         c->draw(shared_from_this(), renderer);
     }
 }
+
+void Node::setName(const std::array<char, 16>& name) { m_name = name; }
+std::array<char, 16> Node::getName() const { return m_name; }
+std::array<char, 16>& Node::getName() { return m_name; }
+
+void Node::setPosition(const Vector3& position) { m_position = position; }
+Vector3 Node::getPosition() const { return m_position; }
+Vector3& Node::getPosition() { return m_position; }
+
+void Node::setSize(const Vector3& size) { m_size = size; }
+Vector3 Node::getSize() const { return m_size; }
+Vector3& Node::getSize() { return m_size; }
 }
