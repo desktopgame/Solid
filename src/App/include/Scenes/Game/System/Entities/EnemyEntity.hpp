2e4b9cd8db2b90a2fb9d019669cabd5592c292fc
desktopgame <gnndr36@gmail.com>
2025-01-01 21:13:42 +0900

    Update: 死亡時にもパーティクル

--- src/App/include/Scenes/Game/System/Entities/EnemyEntity.hpp
+++ src/App/include/Scenes/Game/System/Entities/EnemyEntity.hpp
@@ -8,6 +8,7 @@ public:
 
 protected:
     EnemyEntity(const std::shared_ptr<Common::Graphics::Node>& node);
+    void onDead(const std::shared_ptr<DamageSource>& damageSource) override;
 
 private:
 };
--- src/App/lib/Scenes/Game/System/Entities/EnemyEntity.cpp
+++ src/App/lib/Scenes/Game/System/Entities/EnemyEntity.cpp
@@ -1,3 +1,5 @@
+#include <Common/Graphics/ParticleSystem.hpp>
+#include <Common/Graphics/SphericalParticle.hpp>
 #include <Scenes/Game/System/Entities/EnemyEntity.hpp>
 
 namespace App::Scenes::Game::System::Entities {
@@ -7,5 +9,15 @@ EnemyEntity::~EnemyEntity() { }
 EnemyEntity::EnemyEntity(const std::shared_ptr<Common::Graphics::Node>& node)
     : BasicEntity(node)
 {
+}
+
+void EnemyEntity::onDead(const std::shared_ptr<DamageSource>& damageSource)
+{
+    Common::Graphics::ParticleSystem::emit<Common::Graphics::SphericalParticle>(Common::Graphics::ParticleParameter<Common::Graphics::SphericalParticleOption>(
+        Vector3({ 1.0f, 1.0f, 1.0f }),
+        Vector3({ 0.25f, 0.25f, 0.25f }),
+        0.5f,
+        1.0f,
+        Common::Graphics::SphericalParticleOption(m_position, 1.0f, 20.0f)));
 }
 }
