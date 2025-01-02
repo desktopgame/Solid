6feca23a35ce76d100488c0d170ccf9c59d90a11
desktopgame <gnndr36@gmail.com>
2025-01-02 13:55:57 +0900

    Update: ダメージ値を表示するように

--- src/App/lib/Scenes/Game/System/Entities/EnemyEntity.cpp
+++ src/App/lib/Scenes/Game/System/Entities/EnemyEntity.cpp
@@ -1,6 +1,7 @@
 #include <Common/Graphics/ParticleSystem.hpp>
 #include <Common/Graphics/SphericalParticle.hpp>
 #include <Common/Graphics/TelopSystem.hpp>
+#include <Scenes/Game/System/DamageSource.hpp>
 #include <Scenes/Game/System/Entities/EnemyEntity.hpp>
 
 namespace App::Scenes::Game::System::Entities {
@@ -15,7 +16,10 @@ EnemyEntity::EnemyEntity(const std::shared_ptr<Common::Graphics::Node>& node)
 void EnemyEntity::damage(const std::shared_ptr<DamageSource>& damageSource)
 {
     BasicEntity::damage(damageSource);
-    Common::Graphics::TelopSystem::request(u"A", m_position, Vector3({ 1, 0, 0 }), 1.0f);
+
+    std::string temp = std::to_string(damageSource->getPower());
+    std::u16string text = std::u16string(temp.begin(), temp.end());
+    Common::Graphics::TelopSystem::request(text, m_position, Vector3({ 1, 0, 0 }), 1.0f);
 }
 
 void EnemyEntity::onDead(const std::shared_ptr<DamageSource>& damageSource)
