06512cdaa91f02945f431b14b43909cdff9672e3
desktopgame <gnndr36@gmail.com>
2025-02-10 12:29:00 +0900

    Add: レイアウト処理実装

--- src/App/mod/UI/include/UI/TabbedPane.hpp
+++ src/App/mod/UI/include/UI/TabbedPane.hpp
@@ -1,11 +1,20 @@
 #pragma once
 #include <UI/Container.hpp>
+#include <UI/ILayoutManager.hpp>
 #include <UI/LayoutHint.hpp>
 #include <string>
 
 namespace Lib::UI {
 class TabbedPane : public Container {
 public:
+    class Layout : public ILayoutManager {
+    public:
+        void resizeContainer(const std::shared_ptr<Container>& parent, const Math::Vector2& availableSize) override;
+        void layoutContainer(const std::shared_ptr<Container>& parent) override;
+        Math::Vector2 computePreferredSize(const std::shared_ptr<Container>& parent) override;
+        Math::Vector2 availableSizeFor(const std::shared_ptr<Container>& parent, const std::shared_ptr<Container>& container, const std::shared_ptr<LayoutHint>& hint) override;
+    };
+
     explicit TabbedPane();
 
     void update() override;
--- src/App/mod/UI/lib/UI/TabbedPane.cpp
+++ src/App/mod/UI/lib/UI/TabbedPane.cpp
@@ -1,11 +1,57 @@
 #include <UI/TabbedPane.hpp>
 
 namespace Lib::UI {
+// Layout
+void TabbedPane::Layout::resizeContainer(const std::shared_ptr<Container>& parent, const Math::Vector2& availableSize)
+{
+    Math::Vector2 minimumSize = parent->getMinimumSize();
+    Math::Vector2 maximumSize = parent->getMaximumSize();
+    parent->setSize(Math::Vector2({ Math::Mathf::clamp(minimumSize.x(), maximumSize.x(), availableSize.x()),
+        Math::Mathf::clamp(minimumSize.y(), maximumSize.y(), availableSize.y()) }));
+    parent->setPreferredSize(Math::Vector2({ Math::Mathf::clamp(minimumSize.x(), maximumSize.x(), availableSize.x()),
+        Math::Mathf::clamp(minimumSize.y(), maximumSize.y(), availableSize.y()) }));
+}
+void TabbedPane::Layout::layoutContainer(const std::shared_ptr<Container>& parent)
+{
+    for (int32_t i = 0; i < parent->getLayoutElementCount(); i++) {
+        auto e = parent->getLayoutElementAt(i);
+        e->component->setPosition(Math::Vector2({ 0, -50 }));
+    }
+}
+Math::Vector2 TabbedPane::Layout::computePreferredSize(const std::shared_ptr<Container>& parent)
+{
+    Math::Vector2 preferredSize;
+    for (int32_t i = 0; i < parent->getLayoutElementCount(); i++) {
+        auto e = parent->getLayoutElementAt(i);
+        auto prefSize = e->component->getPreferredSizeSize();
 
+        if (preferredSize.x() < prefSize.x()) {
+            preferredSize.x() = prefSize.x();
+        }
+        if (preferredSize.y() < prefSize.y()) {
+            preferredSize.y() = prefSize.y();
+        }
+    }
+    return preferredSize;
+}
+Math::Vector2 TabbedPane::Layout::availableSizeFor(const std::shared_ptr<Container>& parent, const std::shared_ptr<Container>& container, const std::shared_ptr<LayoutHint>& hint)
+{
+    auto availableSize = container->getPreferredSizeSize();
+    if (Math::Mathf::equals(availableSize.x(), 0.0f) || Math::Mathf::equals(availableSize.y(), 0.0f)) {
+        auto layout = container->getLayout();
+        if (layout) {
+            availableSize = layout->computePreferredSize(container);
+        }
+    }
+    availableSize.y() = Math::Mathf::max(availableSize.y() - 50.0f, 0.0f);
+    return availableSize;
+}
+// public
 TabbedPane::TabbedPane()
     : Container()
     , m_titles()
 {
+    setLayout(std::make_shared<TabbedPane::Layout>());
 }
 
 void TabbedPane::update() { }
