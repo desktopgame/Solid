2931352723c1e672e8a9ccce01381cfc40222d68
desktopgame <gnndr36@gmail.com>
2025-02-15 23:40:05 +0900

    Add: measurePreferredSize() を追加

--- src/App/mod/UI/include/UI/LayoutUtilities.hpp
+++ src/App/mod/UI/include/UI/LayoutUtilities.hpp
@@ -1,4 +1,13 @@
 #pragma once
+#include <UI/Component.hpp>
 
 namespace Lib::UI {
+class LayoutUtilities {
+public:
+    static Math::Vector2 measurePreferredSize(const std::shared_ptr<Component>& c);
+
+private:
+    LayoutUtilities() = delete;
+    ~LayoutUtilities() = delete;
+};
 }
--- src/App/mod/UI/lib/UI/LayoutUtilities.cpp
+++ src/App/mod/UI/lib/UI/LayoutUtilities.cpp
@@ -1,1 +1,20 @@
+#include <UI/Container.hpp>
 #include <UI/LayoutUtilities.hpp>
+
+namespace Lib::UI {
+Math::Vector2 LayoutUtilities::measurePreferredSize(const std::shared_ptr<Component>& c)
+{
+    if (!c) {
+        return Math::Vector2({ 0, 0 });
+    }
+    auto prefSize = c->getPreferredSize();
+    if (Math::Mathf::equals(prefSize.x(), 0.0f) || Math::Mathf::equals(prefSize.y(), 0.0f)) {
+        auto container = std::dynamic_pointer_cast<Container>(c);
+        if (container) {
+            auto layout = container->getLayout();
+            prefSize = layout->computePreferredSize(container);
+        }
+    }
+    return prefSize;
+}
+}
