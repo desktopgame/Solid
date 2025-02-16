d378a69cd6cd12e3e14feb0cb31d22a172215665
desktopgame <gnndr36@gmail.com>
2025-02-16 18:24:58 +0900

    Update: PieceInstanceCollectionを実装

--- src/App/lib/App/Scenes/Game/System/PieceInstanceCollection.cpp
+++ src/App/lib/App/Scenes/Game/System/PieceInstanceCollection.cpp
@@ -1,1 +1,26 @@
 #include <App/Scenes/Game/System/PieceInstanceCollection.hpp>
+
+namespace App::Scenes::Game::System {
+
+PieceInstanceCollection::PieceInstanceCollection()
+    : m_instances()
+{
+}
+
+void PieceInstanceCollection::addInstance(const std::shared_ptr<PieceInstance>& instance)
+{
+    m_instances.emplace_back(instance);
+}
+std::shared_ptr<PieceInstance> PieceInstanceCollection::getInstanceAt(int32_t index) const
+{
+    return m_instances.at(index);
+}
+void PieceInstanceCollection::removeInstanceAt(int32_t index)
+{
+    m_instances.erase(m_instances.begin() + index);
+}
+int32_t PieceInstanceCollection::getInstanceCount() const
+{
+    return static_cast<int32_t>(m_instances.size());
+}
+}
