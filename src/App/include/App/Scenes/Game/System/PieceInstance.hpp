345ff7fe5a6348b0de85b613ae842cd7b0f6bd29
desktopgame <gnndr36@gmail.com>
2025-02-16 18:09:01 +0900

    Update: PieceInstance実装

--- src/App/include/App/Scenes/Game/System/PieceInstance.hpp
+++ src/App/include/App/Scenes/Game/System/PieceInstance.hpp
@@ -1,8 +1,18 @@
 #pragma once
+#include <App/Scenes/Game/System/PieceInfo.hpp>
+#include <App/library.hpp>
+#include <memory>
 
 namespace App::Scenes::Game::System {
 class PieceInstance {
 public:
+    explicit PieceInstance(const std::shared_ptr<PieceInfo>& info, const IntVector2& position);
+
+    std::shared_ptr<PieceInfo> getInfo() const;
+    IntVector2 getPosition() const;
+
 private:
+    std::shared_ptr<PieceInfo> m_info;
+    IntVector2 m_position;
 };
 }
--- src/App/lib/App/Scenes/Game/System/PieceInstance.cpp
+++ src/App/lib/App/Scenes/Game/System/PieceInstance.cpp
@@ -1,4 +1,12 @@
 #include <App/Scenes/Game/System/PieceInstance.hpp>
 
 namespace App::Scenes::Game::System {
+PieceInstance::PieceInstance(const std::shared_ptr<PieceInfo>& info, const IntVector2& position)
+    : m_info(info)
+    , m_position(position)
+{
+}
+
+std::shared_ptr<PieceInfo> PieceInstance::getInfo() const { return m_info; }
+IntVector2 PieceInstance::getPosition() const { return m_position; }
 }
