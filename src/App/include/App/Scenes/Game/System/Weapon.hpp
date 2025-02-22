863efa5d82bcb8f2988f30dad7f5d45bf3c712dc
desktopgame <gnndr36@gmail.com>
2025-02-22 19:45:39 +0900

    Add: Weaponクラスを追加

---
+++ src/App/include/App/Scenes/Game/System/Weapon.hpp
@@ -0,0 +1,40 @@
+#pragma once
+#include <memory>
+
+namespace App::Scenes::Game::System::Entities {
+class PlayerEntity;
+}
+
+namespace App::Scenes::Game::System {
+class Field;
+class Weapon {
+public:
+    class Parameter {
+    public:
+        int32_t power;
+        float fireRate;
+        float coolRate;
+        float increaseEnergy;
+        float decreaseEnergy;
+    };
+
+    enum class InputMethod {
+        None,
+        LeftOneShot,
+        LeftContinuous,
+        RightOneShot,
+        RightContinuous
+    };
+
+    explicit Weapon(const Parameter& parameter);
+    virtual ~Weapon();
+
+    virtual void execute(const std::shared_ptr<Field>& field, const std::shared_ptr<Entities::PlayerEntity>& player) = 0;
+
+    virtual InputMethod getInputMethod() const = 0;
+    virtual float getEnergyMax() const = 0;
+
+protected:
+    const Parameter m_parameter;
+};
+}
---
+++ src/App/lib/App/Scenes/Game/System/Weapon.cpp
@@ -0,0 +1,10 @@
+#include <App/Scenes/Game/System/Weapon.hpp>
+
+namespace App::Scenes::Game::System {
+
+Weapon::Weapon(const Parameter& parameter)
+    : m_parameter(parameter)
+{
+}
+Weapon::~Weapon() { }
+}
