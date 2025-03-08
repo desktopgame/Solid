af51d26c291e25f5141f5c3c1c3e59273b73dd6c
desktopgame <gnndr36@gmail.com>
2025-03-08 11:24:10 +0900

    Update: プレーンパーティクルを追加

---
+++ src/App/include/App/Common/Graphics/PlaneParticle.hpp
@@ -0,0 +1,45 @@
+#pragma once
+#include <App/Common/Graphics/ParticleBase.hpp>
+#include <App/library.hpp>
+#include <random>
+
+namespace App::Common::Graphics {
+
+struct PlaneParticleOption {
+    Vector3 floorPoint;
+    Vector3 size;
+
+    PlaneParticleOption(const Vector3& floorPoint, Vector3 size)
+        : floorPoint(floorPoint)
+        , size(size)
+    {
+    }
+};
+
+class PlaneParticle : public ParticleBase<PlaneParticleOption> {
+public:
+    using Option = PlaneParticleOption;
+    static inline constexpr IParticle::Type Type = IParticle::Type::Spherical;
+
+    explicit PlaneParticle()
+        : ParticleBase()
+    {
+    }
+
+protected:
+    void batch(std::array<VertexParticle3D, NumParticles>& particles, const ParticleParameter<PlaneParticleOption>& params) override
+    {
+        Random random;
+        for (auto& particle : particles) {
+            float fx = random.range(-(params.options.size.x() / 2.0f), (params.options.size.x() / 2.0f));
+            float fz = random.range(-(params.options.size.z() / 2.0f), (params.options.size.z() / 2.0f));
+            float speed = 20.5f;
+            Vector3 direction = Vector3({ 0, 1, 0 });
+
+            particle.offset = params.options.floorPoint + Vector3({ fx, 0, fz });
+            particle.velocity = direction * speed;
+            particle.lifetime = random.range(params.minLifetime, params.maxLifetime);
+        }
+    }
+};
+}
