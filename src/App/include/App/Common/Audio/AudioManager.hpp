07ad103ac6c7636d9848bd15a52ebda444c7438d
desktopgame <gnndr36@gmail.com>
2025-03-09 16:37:03 +0900

    Add: AudioManagerを追加

---
+++ src/App/include/App/Common/Audio/AudioManager.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include <App/library.hpp>
+#include <memory>
+#include <unordered_map>
+#include <vector>
+
+namespace App::Common::Audio {
+class AudioManager {
+public:
+    static std::shared_ptr<AudioManager> getInstance();
+    static void destroyInstance();
+    ~AudioManager();
+
+    std::shared_ptr<AudioClip> loadClip(const std::string& file);
+
+    void playBgm(const std::string& file);
+    void playSE(const std::string& file);
+
+private:
+    AudioManager();
+    void init();
+
+    static std::shared_ptr<AudioManager> s_instance;
+
+    std::shared_ptr<AudioSource> m_bgmSource;
+    std::vector<std::shared_ptr<AudioSource>> m_seSources;
+    std::unordered_map<std::string, std::shared_ptr<AudioClip>> m_clipMap;
+};
+}
---
+++ src/App/lib/App/Common/Audio/AudioManager.cpp
@@ -0,0 +1,75 @@
+#include <App/Common/Audio/AudioManager.hpp>
+
+namespace App::Common::Audio {
+std::shared_ptr<AudioManager> AudioManager::s_instance;
+
+std::shared_ptr<AudioManager> AudioManager::getInstance()
+{
+    if (!s_instance) {
+        s_instance = std::shared_ptr<AudioManager>(new AudioManager());
+        s_instance->init();
+    }
+    return s_instance;
+}
+
+void AudioManager::destroyInstance()
+{
+    s_instance = nullptr;
+}
+
+AudioManager::~AudioManager()
+{
+}
+
+std::shared_ptr<AudioClip> AudioManager::loadClip(const std::string& file)
+{
+    if (m_clipMap.count(file) == 0) {
+        auto clip = AudioClip::create();
+        clip->loadWav(file);
+        m_clipMap.insert_or_assign(file, clip);
+    }
+    return m_clipMap.at(file);
+}
+
+void AudioManager::playBgm(const std::string& file)
+{
+    m_bgmSource->stop();
+
+    m_bgmSource->setClip(loadClip(file));
+    m_bgmSource->play();
+}
+void AudioManager::playSE(const std::string& file)
+{
+    for (int32_t i = 0; i < static_cast<int32_t>(m_seSources.size()); i++) {
+        auto seSource = m_seSources.at(i);
+        if (!seSource->getClip() || !seSource->isPlaying()) {
+            seSource->stop();
+
+            seSource->setClip(loadClip(file));
+            seSource->play();
+            break;
+        }
+    }
+}
+// private
+AudioManager::AudioManager()
+    : m_bgmSource()
+    , m_seSources()
+    , m_clipMap()
+{
+}
+
+void AudioManager::init()
+{
+    m_bgmSource = AudioSource::create();
+    m_bgmSource->setLoop(true);
+    m_bgmSource->setVolume(0.5f);
+
+    for (int32_t i = 0; i < 10; i++) {
+        auto seSource = AudioSource::create();
+        seSource->setLoop(false);
+        seSource->setVolume(1.0f);
+        m_seSources.emplace_back(seSource);
+    }
+}
+}
