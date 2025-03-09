553362239155b495a848d1304d3685b88d08aff8
desktopgame <gnndr36@gmail.com>
2025-03-09 15:16:11 +0900

    Update: 初期化コードを追加

--- src/App/mod/Audio/include/Audio/AudioSource.hpp
+++ src/App/mod/Audio/include/Audio/AudioSource.hpp
@@ -1,9 +1,16 @@
 #pragma once
+#include <memory>
 
 namespace Lib::Audio {
 class AudioSource {
 public:
+    static std::shared_ptr<AudioSource> create();
+    ~AudioSource();
+
 private:
     AudioSource();
+    void init();
+
+    uint32_t m_id;
 };
 }
--- src/App/mod/Audio/lib/Audio/AudioSource.cpp
+++ src/App/mod/Audio/lib/Audio/AudioSource.cpp
@@ -1,2 +1,25 @@
+#include <AL/al.h>
+#include <Audio/AudioSource.hpp>
+
 namespace Lib::Audio {
+std::shared_ptr<AudioSource> AudioSource::create()
+{
+    auto ret = std::shared_ptr<AudioSource>(new AudioSource());
+    ret->init();
+    return ret;
+}
+AudioSource::~AudioSource()
+{
+    alDeleteSources(1, &m_id);
+}
+// private
+AudioSource::AudioSource()
+    : m_id()
+{
+}
+
+void AudioSource::init()
+{
+    alGenSources(1, &m_id);
+}
 }
