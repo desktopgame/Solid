02a75a1fe11acfc627b5860b44b6958f7852e6f4
desktopgame <gnndr36@gmail.com>
2025-03-09 15:44:09 +0900

    Update: 読み込み実装

--- src/App/mod/Audio/include/Audio/AudioClip.hpp
+++ src/App/mod/Audio/include/Audio/AudioClip.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <memory>
+#include <string>
 
 namespace Lib::Audio {
 class AudioClip {
@@ -7,10 +8,17 @@ public:
     static std::shared_ptr<AudioClip> create();
     ~AudioClip();
 
+    void loadWav(const std::string& file);
+
 private:
     AudioClip();
     void init();
+    void releaseData();
+
+    static bool isBigEndian();
+    static int32_t convertToInt(char* buffer, int32_t len);
 
     uint32_t m_id;
+    char* m_data;
 };
 }
--- src/App/mod/Audio/lib/Audio/AudioClip.cpp
+++ src/App/mod/Audio/lib/Audio/AudioClip.cpp
@@ -1,5 +1,7 @@
 #include <AL/al.h>
 #include <Audio/AudioClip.hpp>
+#include <fstream>
+#include <stdexcept>
 
 namespace Lib::Audio {
 std::shared_ptr<AudioClip> AudioClip::create()
@@ -11,15 +13,92 @@ std::shared_ptr<AudioClip> AudioClip::create()
 AudioClip::~AudioClip()
 {
     alDeleteBuffers(1, &m_id);
+    releaseData();
 }
+
+void AudioClip::loadWav(const std::string& file)
+{
+    releaseData();
+    // see: https://stackoverflow.com/questions/36949957/loading-a-wav-file-for-openal
+    char buffer[4];
+    std::ifstream in(file, std::ios::binary);
+    in.read(buffer, 4);
+    if (strncmp(buffer, "RIFF", 4) != 0) {
+        throw std::invalid_argument("illegal format.");
+    }
+    in.read(buffer, 4);
+    in.read(buffer, 4); // WAVE
+    in.read(buffer, 4); // fmt
+    in.read(buffer, 4); // 16
+    in.read(buffer, 2); // 1
+    in.read(buffer, 2);
+    int32_t chan = convertToInt(buffer, 2);
+    in.read(buffer, 4);
+    int32_t samplerate = convertToInt(buffer, 4);
+    in.read(buffer, 4);
+    in.read(buffer, 2);
+    in.read(buffer, 2);
+    int32_t bps = convertToInt(buffer, 2);
+    in.read(buffer, 4); // data
+    in.read(buffer, 4);
+    int32_t size = convertToInt(buffer, 4);
+    m_data = new char[size];
+    in.read(m_data, size);
+
+    ALenum format;
+    if (chan == 1) {
+        if (bps == 8) {
+            format = AL_FORMAT_MONO8;
+        } else {
+            format = AL_FORMAT_MONO16;
+        }
+    } else {
+        if (bps == 8) {
+            format = AL_FORMAT_STEREO8;
+        } else {
+            format = AL_FORMAT_STEREO16;
+        }
+    }
+    alBufferData(m_id, format, m_data, size, samplerate);
+}
+
 // private
 AudioClip::AudioClip()
     : m_id()
+    , m_data(nullptr)
 {
 }
 
 void AudioClip::init()
 {
     alGenBuffers(1, &m_id);
+}
+
+void AudioClip::releaseData()
+{
+    if (m_data) {
+        delete m_data;
+        m_data = nullptr;
+    }
+}
+
+bool isBigEndian()
+{
+    // see: https://stackoverflow.com/questions/36949957/loading-a-wav-file-for-openal
+    int32_t a = 1;
+    return !((char*)&a)[0];
+}
+
+int32_t convertToInt(char* buffer, int32_t len)
+{
+    // see: https://stackoverflow.com/questions/36949957/loading-a-wav-file-for-openal
+    int32_t a = 0;
+    if (!isBigEndian())
+        for (int32_t i = 0; i < len; i++)
+            ((char*)&a)[i] = buffer[i];
+    else
+        for (int32_t i = 0; i < len; i++)
+            ((char*)&a)[3 - i] = buffer[i];
+    return a;
 }
 }
